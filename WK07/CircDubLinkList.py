5dc9d041afc371ea931a9a84909ac58730639466
Mason L <sux462@gmail.com>
2024-10-07 00:43:18 -0700

    Cir Dub Link List seems to be working now

--- WK07/CircDubLinkList.py
+++ WK07/CircDubLinkList.py
@@ -4,76 +4,81 @@ class MyNode:
         self.next = None
         self.prev = None
 
-class CircularlyLinkedList:
+class CircularDoublyLinkedList:
     def __init__(self, _data):
         self.head = MyNode(_data)
+        self.head.prev = self.head
+        self.head.next = self.head
         self.tail = self.head
+
         if _data is None:
             self.length = 0
         else:
             self.length = 1
 
+    def forwardSearch(self, _data):
+        currentNode = self.head
+        count = 0
+        while count < self.length:
+            if currentNode.data == _data:
+                return currentNode
+            currentNode = currentNode.next
+            count += 1
+        return None
+
     def append(self, _data):
         if self.head == None:
             self.head = MyNode(_data)
+            self.head.prev = self.head
+            self.head.next = self.head
             self.tail = self.head
             return
         else:
             self.tail.next = MyNode(_data)
             self.tail.next.prev = self.tail
             self.tail = self.tail.next
+            self.tail.next = self.head
+            self.head.prev = self.tail
         self.length += 1
-
-    def dis(self): # display
-        if self.head is None:
-            print('CLL[]')
-            return
-        node = self.head
-        print('CLL[' + str(node.data), end='')
-        if node.next is None:
-            print(']')
-        while node.next is not None:
-            node = node.next
-            if node.next is None:
-                print(f', {str(node.data)}', end=']\n')
-            else:
-                print(f', {str(node.data)}', end='')
-
-    def forwardSearch(self, _data):
-        currentNode = self.head
-        while currentNode is not None:
-            if currentNode.data == _data:
-                return currentNode
-            currentNode = currentNode.next
-        return None
 
     def remove(self, _data):
         currentNode = self.forwardSearch(_data)
         if currentNode is None: # data searched for not found
             print(f'{_data} not found, remove operation failed.')
             return
-        elif currentNode.next is None: # handles edge case: tail
-            currentNode.prev.next = None
-            self.tail = currentNode.prev
-            del currentNode
-            
-        elif currentNode.prev is None: # handles edge case: head
-            currentNode.next.prev = None
-            self.head = currentNode.next
-            del currentNode
-            
-        else: # handles removal of any other position within the linked list
+        else:
             currentNode.prev.next = currentNode.next
             currentNode.next.prev = currentNode.prev
+            if currentNode is self.head:
+                self.head = currentNode.next
+            elif currentNode is self.tail:
+                self.tail = currentNode.prev
             del currentNode
         self.length -= 1
 
-x = CircularlyLinkedList(0)
+    def dis(self): # display
+        if self.head is None:
+            print('CDLL[]')
+            return
+        node = self.head
+        print('CDLL[' + str(node.data), end='')
+        if node.next is self.head:
+            print(']')
+        count = 0
+        while count < self.length-1:
+            node = node.next
+            if node.next is not self.head:
+                print(f', {str(node.data)}', end='')
+            else:
+                print(f', {str(node.data)}', end=']\n')
+            count += 1
+
+x = CircularDoublyLinkedList(0)
 
 for i in range(1, 10, 1):
     x.append(i)
 
-print('Initial Doubly Linked List')
+print('Initial Circular Doubly Linked List')
 x.dis()
 print(f'Head: {x.head.data} \tTail: {x.tail.data}  \tLen: {x.length}\n')
 
